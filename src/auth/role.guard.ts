dfda937d0f2f33bbae3eeb1e5e832a0af83636ab
Krystian Domzalowicz <krystian.domza@gmail.com>
2023-04-25 19:11:44 +0200

    feat: add route protection: verify users role

---
+++ src/auth/role.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { Role } from 'src/common/enums';
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const requiredRole: Role = this.reflector.getAllAndOverride<Role>('role', [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+
+    if (!requiredRole) {
+      return true;
+    }
+
+    const { user } = context.switchToHttp().getRequest();
+
+    return user.role === requiredRole;
+  }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,29 @@
 import { Controller, Get, Request, UseGuards } from '@nestjs/common';
+import { HasRole } from 'src/auth/decorators/has-role.decorator';
 
 import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { RoleGuard } from 'src/auth/role.guard';
+import { Role } from 'src/common/enums';
 
-@Controller('users')
+@Controller()
 export class UsersController {
   @UseGuards(JwtAuthGuard)
   @Get('profile')
   getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @HasRole(Role.trainer)
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Get('trainer')
+  onlyTrainer(@Request() req) {
+    return req.user;
+  }
+
+  @HasRole(Role.user)
+  @UseGuards(JwtAuthGuard, RoleGuard)
+  @Get('user')
+  onlyUser(@Request() req) {
     return req.user;
   }
 }
