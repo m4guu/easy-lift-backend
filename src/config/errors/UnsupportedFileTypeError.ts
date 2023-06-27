8e66fd33ebd26adc03d5b849f13cda953a3f4a1e
Krystian Domzalowicz <krystian.domza@gmail.com>
2023-06-27 20:27:41 +0200

    feat: add handling erors in programs controllers & services

---
+++ src/config/errors/UnsupportedFileTypeError.ts
@@ -0,0 +1,12 @@
+import { HttpStatus } from '@nestjs/common';
+import { DomainError, ErrorId } from 'src/libs/errors';
+
+export class UnsupportedFileTypeError extends DomainError {
+  constructor() {
+    super(
+      'Unsupported file type.',
+      HttpStatus.BAD_REQUEST,
+      ErrorId.UNSUPPORTED_FILE_TYPE,
+    );
+  }
+}
