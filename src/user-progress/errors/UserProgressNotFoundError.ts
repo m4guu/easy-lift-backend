4443bd488e3c8e3bdf11a9d939e5077e1f2e2d0f
Krystian Domzalowicz <krystian.domza@gmail.com>
2023-06-27 21:08:56 +0200

    feat: add handling erors in workout/user progress controllers & services

---
+++ src/user-progress/errors/UserProgressNotFoundError.ts
@@ -0,0 +1,12 @@
+import { HttpStatus } from '@nestjs/common';
+import { DomainError, ErrorId } from 'src/libs/errors';
+
+export class UserProgressNotFoundError extends DomainError {
+  constructor() {
+    super(
+      'User progress not found.',
+      HttpStatus.NOT_FOUND,
+      ErrorId.USER_PROGRESS_NOT_FOUND,
+    );
+  }
+}
---
+++ src/workouts/errors/WokroutNotFoundError.ts
@@ -0,0 +1,12 @@
+import { HttpStatus } from '@nestjs/common';
+import { DomainError, ErrorId } from 'src/libs/errors';
+
+export class WorkoutNotFoundError extends DomainError {
+  constructor() {
+    super(
+      'Workout not found.',
+      HttpStatus.NOT_FOUND,
+      ErrorId.WORKOUT_NOT_FOUND,
+    );
+  }
+}
